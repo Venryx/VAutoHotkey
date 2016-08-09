af809abdc034ee735fc5da75f37b35e38ec97daa
Venryx <venryx@gmail.com>
2016-08-08 17:58:47 -0700

    *

--- ScriptExamples/Default.js
+++ ScriptExamples/Default.js
@@ -4,7 +4,7 @@ CreateTrayIcon();
 AddGlobalHotkey("Control+Shift+Escape", {capture: true, onDown: function(data) {
 	// if explorer isn't running yet, start it
 	if (!IsProcessOpen("explorer"))
-        Run("C:\\Windows\\explorer.exe");
+        Run("C:/Windows/explorer.exe");
 	// else, find its first window, then show and activate it
     else {
         var window = GetWindow({processName: "explorer.exe", class: "CabinetWClass"});
